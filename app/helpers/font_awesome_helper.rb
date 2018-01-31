13a0b395fb2d3543d34488923ea4c079d6999805
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-31 19:10:20 -0300

    added FontAwesomeHelper#boolean_to_gylph helper

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -4,6 +4,13 @@ module FontAwesomeHelper
     link_to(icon_with_label(fa_icon_name, label, size), url, options)
   end
 
+  def boolean_to_glyph(value, options = {} )
+    size = options[:size] || 16
+    icon_name = value ? 'check' : 'times'
+    icon_color = value ? 'green' : 'red'
+    fa_icon icon_name, style: "font-size:#{size}px; color:#{icon_color};"
+  end
+
   private
   def icon_with_label(icon_name, label, size)
     fa_icon icon_name, text: label, style: "font-size:#{size}px;"
