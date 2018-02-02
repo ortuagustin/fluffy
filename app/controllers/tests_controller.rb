e09c489d522467b73e403f2f47f1bf639fd2d0a2
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-02 15:37:32 -0300

    pagination for tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,7 +4,7 @@ class TestsController < ApplicationController
 
   # GET /courses/:course_id/tests
   def index
-    @tests = Test.from_course_id(course_id)
+    @tests = Test.from_course_id(course_id).page(params[:page])
   end
 
   # GET /courses/:course_id/tests/new
