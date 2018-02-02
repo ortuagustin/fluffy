ded29a6e2e750ad57a0ddd9a62429aac660d1dfb
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-02 01:18:18 -0300

    Courses: improved rendering of the courses index view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,52 +1,58 @@
 class CoursesController < ApplicationController
+  before_action :set_colours
   before_action :set_course, only: [:show, :destroy]
   before_action :fetch_courses, only: [:index, :create]
+  helper_method :tile_class
 
   # GET /courses
-  # GET /courses.json
   def index
     @course = Course.new
   end
 
   # GET /courses/1
-  # GET /courses/1.json
   def show
     redirect_to course_students_path(course_id)
   end
 
   # POST /courses
-  # POST /courses.json
   def create
     @course = Course.new(course_params)
 
-    respond_to do |format|
-      if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
-        format.json { render :show, status: :created, location: @course }
-      else
-        format.html { render :index }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
-      end
+    if @course.save
+      redirect_to @course, notice: 'Course was successfully created.'
+    else
+      render :index
     end
   end
 
   # DELETE /courses/1
-  # DELETE /courses/1.json
   def destroy
     @course.destroy
-    respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to courses_url, notice: 'Course was successfully destroyed.'
+  end
+private
+  def set_colours
+    @colours = ['success', 'warning', 'danger', 'info', 'link']
   end
 
-private
+  def random_colour(index)
+    @colours.shuffle[index]
+  end
+
+  def colour(index)
+    random_colour(index % @colours.size)
+  end
+
+  def tile_class(index)
+    "'tile is-child box notification has-text-centered is-#{colour(index)}'".html_safe
+  end
+
   def set_course
     @course = Course.find(params[:id])
   end
 
   def fetch_courses
-    @courses = Course.all
+    @courses = Course.order(year: :desc)
   end
 
   def course_id
