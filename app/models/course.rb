2a627998f5a885bd2ba7299400df16799692fed0
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-04 21:16:16 -0300

    Course: specifying the associations order in the has_many method call, so it does not interfere with seeds

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,19 +1,12 @@
 class Course < ApplicationRecord
-  has_many :students, dependent: :destroy
-  has_many :tests, dependent: :destroy
+  has_many :students, -> { order(:surname, :name) }, dependent: :destroy
+  has_many :tests, -> { order('evaluated_at ASC') }, dependent: :destroy
+
   validates_associated :students
   validates_associated :tests
   validates :year, presence: true
   validates :year, uniqueness: true
   validates :year, format: { with: /(19|20)\d{2}/i, message: :invalid_course_year }
-
-  def students
-    super.order(:surname, :name)
-  end
-
-  def tests
-    super.order(evaluated_at: :asc)
-  end
 
   def attendants_for(test)
     students.select { |each| each.attended_to? test }.count
