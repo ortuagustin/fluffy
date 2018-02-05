e36a99b7e77d6ce0d7ad4d2f9863df659698dc79
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-05 16:35:59 -0300

    added locale scope for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: :devise_controller?
 
   protected
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:username, :email, :email_confirmation, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
-  resources :courses, except: [:edit, :update, :new] do
-    get 'summary', to: 'courses#summary', on: :member, as: :summary
-    resources :students, except: [:show]
+    get 'home', to: 'courses#index', as: :home
+    resources :courses, except: [:edit, :update, :new] do
+      get 'summary', to: 'courses#summary', on: :member, as: :summary
+      resources :students, except: [:show]
 
-    resources :tests, except: [:show] do
-      get 'califications', to: 'califications#show', on: :member
-      put 'califications', to: 'califications#update', on: :member
-      get 'edit_califications', to: 'califications#edit', on: :member
+      resources :tests, except: [:show] do
+        get 'califications', to: 'califications#show', on: :member
+        put 'califications', to: 'califications#update', on: :member
+        get 'edit_califications', to: 'califications#edit', on: :member
+      end
     end
   end
 
   authenticated :user do
-    root to: 'courses#index', as: :authenticated_root
+    root to: redirect("#{I18n.locale}/home/"), as: :authenticated_root
   end
 
-  root to: redirect('/users/login')
+  root to: redirect("#{I18n.locale}/users/login")
 end
