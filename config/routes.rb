5ec0872c3ef334e6a540ac23183929c282b5ad62
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-05 18:48:30 -0300

    added paginatable concern in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
     get 'home', to: 'courses#index', as: :home
     resources :courses, except: [:edit, :update, :new] do
       get 'summary', to: 'courses#summary', on: :member, as: :summary
-      resources :students, except: [:show]
+      resources :students, except: [:show], concerns: :paginatable
 
-      resources :tests, except: [:show] do
+      resources :tests, except: [:show], concerns: :paginatable do
         get 'califications', to: 'califications#show', on: :member
         put 'califications', to: 'califications#update', on: :member
         get 'edit_califications', to: 'califications#edit', on: :member
