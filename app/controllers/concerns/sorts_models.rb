317084f37eaafb4ad658ae14e77e2e48bb2a83a9
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-06 15:17:53 -0300

    Added helper methods to SortsModel concern to query if sort arguments are present

--- app/controllers/concerns/sorts_models.rb
+++ app/controllers/concerns/sorts_models.rb
@@ -3,7 +3,7 @@ module SortsModels
 
   included do
     before_action :set_params
-    helper_method :sort_column, :sort_direction
+    helper_method :sort_column, :sort_direction, :sort_column?, :sort_direction?
   end
 
   def set_params
@@ -12,23 +12,31 @@ module SortsModels
     @direction = p[:direction]
   end
 
+  def sort_column
+    sort_column? ? @sort : default_sort_column
+  end
+
+  def sort_direction
+    sort_direction? ? @direction : default_sort_direction
+  end
+
+  def sort_params
+    sort_column + ' ' + sort_direction
+  end
+
+  def sort_column?
+    sortable_columns.include?(@sort)
+  end
+
+  def sort_direction?
+    %w[asc desc].include?(@direction)
+  end
+
   def default_sort_column
     ''
   end
 
   def default_sort_direction
     'asc'
-  end
-
-  def sort_column
-    sortable_columns.include?(@sort) ? @sort : default_sort_column
-  end
-
-  def sort_direction
-    %w[asc desc].include?(@direction) ? @direction : default_sort_direction
-  end
-
-  def sort_params
-    sort_column + ' ' + sort_direction
   end
 end
