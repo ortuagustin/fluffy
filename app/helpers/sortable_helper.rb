8773e7a9759e6766c34b459f514449cd1d7f02c4
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-05 21:40:07 -0300

    Adding SortableHelper module to be used in the Views to generate table headers that are clickable and send GET request with sorting args

---
+++ app/helpers/sortable_helper.rb
@@ -0,0 +1,18 @@
+module SortableHelper
+  def sortable(attribute, title, options = {})
+    link_to column_title(attribute, title), { sort: attribute, direction: direction(attribute) }, options
+  end
+
+  def column_title(attribute, title)
+    return title unless attribute == sort_column
+    "#{title} #{sort_icon(attribute)}".html_safe
+  end
+
+  def sort_icon(attribute)
+    fa_icon "sort-#{direction(attribute)}"
+  end
+
+  def direction(attribute)
+    attribute == sort_column && sort_direction == "asc" ? "desc" : "asc"
+  end
+end
