33a69513f541d747db1974fdf5dcbc28cc8189be
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-05 21:39:28 -0300

    SortsModels concern: added two hooks to customize the default_sort_column and direction when they are missing in the params of the request

--- app/controllers/concerns/sorts_models.rb
+++ app/controllers/concerns/sorts_models.rb
@@ -5,12 +5,20 @@ module SortsModels
     helper_method :sort_column, :sort_direction
   end
 protected
+  def default_sort_column
+    ''
+  end
+
+  def default_sort_direction
+    'asc'
+  end
+
   def sort_column
-    sortable_columns.include?(params[:sort]) ? params[:sort] : ''
+    sortable_columns.include?(params[:sort]) ? params[:sort] : default_sort_column
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : ''
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : default_sort_direction
   end
 
   def sort_params
