353c8616da4582f8c0f1fcfaa59f907dadc3787d
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-07 14:56:23 -0300

    courses#summary: pagination urls will now look like /summary/page/n instead of /summary/?page=n

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
+  concern :paginates_summary do
+    get 'summary/(page/:page)', action: :summary, on: :member, as: ''
+  end
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
     get 'home', to: 'courses#index', as: :home
-    resources :courses, except: [:edit, :update, :new] do
+    resources :courses, except: [:edit, :update, :new], concerns: :paginates_summary do
       get 'summary', to: 'courses#summary', on: :member, as: :summary
       resources :students, except: [:show], concerns: :paginatable
 
