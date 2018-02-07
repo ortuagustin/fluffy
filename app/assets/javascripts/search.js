52b299a00e32e7ae2cbc60d5016548e26ded8acb
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-06 23:59:30 -0300

    removed a console.log from search.js search.js shoul be included when rendering search_form

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,5 @@
 $(function() {
   $("#search input").keyup(function() {
-    console.log($("#search").serialize());
     $.get($("#search").attr("action"), $("#search").serialize(), null, "script");
     return false;
   });
