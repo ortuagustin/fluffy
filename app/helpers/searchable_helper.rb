8e1b72f315ebf217f870cee36188cbed06ac7d49
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-07 01:23:54 -0300

    SearchableHelper now supports rendering remote_search_form (sends ajax request on typing) and search_form where the user must submit de form manually

--- app/helpers/searchable_helper.rb
+++ app/helpers/searchable_helper.rb
@@ -1,5 +1,10 @@
 module SearchableHelper
-  def search_form(url, &block)
-    render('application/search', &block)
+  def remote_search_form(url, &block)
+    search_form(url, true, &block)
+  end
+
+  def search_form(url, remote = false, &block)
+    form_id = remote ? 'search' : nil
+    render layout: 'application/search', locals: { url: url, remote: remote, form_id: form_id }, &block
   end
 end
