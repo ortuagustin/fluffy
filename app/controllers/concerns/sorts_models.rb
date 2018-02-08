96a4c39586ab773a228da84b854708d38861e88c
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-08 08:42:28 -0300

    Improved the implementation of SortsModels:- Provides a class method :sorts that receives the name of a model (tableized version, ie, 'students', 'courses') and generates methods "sort_name_by_field" for each specified attribute. Those links are analog to old SortableHelper#sortable

--- app/controllers/concerns/sorts_models.rb
+++ app/controllers/concerns/sorts_models.rb
@@ -2,41 +2,95 @@ module SortsModels
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_params
-    helper_method :sort_column, :sort_direction, :sort_column?, :sort_direction?
+    helper_method :sort_column, :sort_direction, :sort_column?, :sort_direction?, :sort_form_inputs
   end
 
-  def set_params
-    p = params.permit(:sort, :direction)
-    @sort = p[:sort]
-    @direction = p[:direction]
+  def sort_form_inputs
+    fields = ''
+    fields << (view_context.hidden_field_tag :sort, sort_column) if sort_column?
+    fields << (view_context.hidden_field_tag :direction, sort_direction) if sort_direction?
+    fields.html_safe
+  end
+
+  def sanitized_sort_params
+    params.permit(:sort, :direction)
   end
 
   def sort_column
-    sort_column? ? @sort : default_sort_column
+    sort_column? ? sanitized_sort_params[:sort] : default_sort_column
+  end
+
+  def sort_direction?
+    %w[asc desc].include?(sanitized_sort_params[:direction])
   end
 
   def sort_direction
-    sort_direction? ? @direction : default_sort_direction
+    sort_direction? ? sanitized_sort_params[:direction] : default_sort_direction
+  end
+
+  def default_sort_direction
+    'asc'
   end
 
   def sort_params
     sort_column + ' ' + sort_direction
   end
 
-  def sort_column?
-    sortable_columns.include?(@sort)
+  module ClassMethods
+    def sorts(model, default = nil, *fields)
+      resource = model.to_s.tableize
+      @@fields = model_fields(resource, fields)
+      create_sort_by_methods(resource, @@fields)
+      create_helpers(fields, default)
+    end
+
+    private
+      def create_helpers(fields, default)
+        define_method(:sort_column?) do
+          @@fields.include?(sanitized_sort_params[:sort])
+        end
+
+        define_method(:default_sort_column) do
+          default.blank? ? @@fields.first : default.to_s
+        end
+      end
+
+      def create_sort_by_methods(resource, fields)
+        fields.each do |field|
+          method_name = "sort_#{resource}_by_#{field}".to_sym
+          define_method(method_name) do
+            view_context.link_to header(resource, field), url(field), remote: true
+          end
+
+          helper_method method_name
+        end
+      end
+
+      def model_fields(resource, fields = [])
+        return get_model_class(resource.classify).column_names if fields.blank?
+        fields.map {|f| f.to_s }
+      end
+
+      def get_model_class(klass)
+        Object.const_get klass
+      end
+  end
+private
+  def header(name, field)
+    h = I18n.t("#{name}.fields.#{field}")
+    return h unless field == sort_column
+    "#{h} #{icon(field)}".html_safe
   end
 
-  def sort_direction?
-    %w[asc desc].include?(@direction)
+  def url(field)
+    { q: filter, sort: field, direction: direction(field) }
   end
 
-  def default_sort_column
-    ''
+  def direction(field)
+    field == sort_column && sort_direction == "asc" ? "desc" : "asc"
   end
 
-  def default_sort_direction
-    'asc'
+  def icon(field)
+    view_context.fa_icon "sort-#{direction(field)}"
   end
 end
