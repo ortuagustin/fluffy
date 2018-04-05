68aa027f54de889c59fc1496cbdc7387573b47c9
Agustin Ortu <ortu.agustin@gmail.com>
2018-04-05 15:30:32 -0300

    typo

--- test/controllers/replies_controller_test.rb
+++ test/controllers/replies_controller_test.rb
@@ -1,22 +1,20 @@
 require 'test_helper'
 
 class RepliesControllerTest < ActionDispatch::IntegrationTest
-  require 'test_helper'
-
   setup do
     @course = courses(:current_course)
     @post = posts(:one)
     @user = users(:teacher)
   end
 
-  test "unauthorized users cannot reply to posts " do
+  test "unauthorized users cannot reply to posts" do
     assert_no_difference "Reply.count" do
       post course_post_replies_path(@course, @post), params: { reply: reply_params }
       assert_redirected_to '/users/login'
     end
   end
 
-  test "authorized users can see posts" do
+  test "authorized users can reply to posts" do
     login_as @user
 
     assert_difference('Reply.count') do
