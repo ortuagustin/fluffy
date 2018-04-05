b40012b0832c61b7dbda0a53029191efe5b3807c
Agustin Ortu <ortu.agustin@gmail.com>
2018-04-05 15:25:24 -0300

    Added missing translations

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,6 +1,6 @@
 module PostHelper
   def time_since_posted(post)
-    "#{distance_of_time_in_words_to_now(post.created_at)} ago..."
+    "#{distance_of_time_in_words_to_now(post.created_at)} ..."
   end
 
   def post_body(post, truncate)
