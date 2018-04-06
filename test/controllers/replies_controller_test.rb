64393b6c17c184ed7ef0c31a82c93d52d7d1d164
agustin <ortu.agustin@gmail.com>
2018-04-06 05:21:43 -0300

    fixed replies_controller_test

--- test/controllers/replies_controller_test.rb
+++ test/controllers/replies_controller_test.rb
@@ -1,26 +1,27 @@
 require 'test_helper'
 
 class RepliesControllerTest < ActionDispatch::IntegrationTest
-  require 'test_helper'
-
   setup do
     @course = courses(:current_course)
     @post = posts(:one)
     @user = users(:teacher)
   end
 
-  test "unauthorized users cannot reply to posts " do
+  test "unauthorized users cannot reply to posts" do
     assert_no_difference "Reply.count" do
       post course_post_replies_path(@course, @post), params: { reply: reply_params }
       assert_redirected_to '/users/login'
+
+      post course_post_replies_path(@course, @post), params: { reply: reply_params, format: 'json' }
+      assert_response :unauthorized
     end
   end
 
-  test "authorized users can see posts" do
+  test "authorized users can reply to posts" do
     login_as @user
 
     assert_difference('Reply.count') do
-      post course_post_replies_path(@course, @post), params: { reply: reply_params }
+      post course_post_replies_path(@course, @post), params: { reply: reply_params, format: 'json' }
       assert_response :success
     end
   end
