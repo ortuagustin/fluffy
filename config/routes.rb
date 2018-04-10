4447d8e742f20c84472d3463c289d5c44b49703b
Agustin Ortu <ortu.agustin@gmail.com>
2018-04-10 12:03:37 -0300

    dry routes with concerns

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
+  concern :likeable do |options|
+    post 'like', action: :create, controller: options[:controller], on: :member
+    delete 'like', action: :destroy, controller: options[:controller], on: :member
+  end
+
+  concern :dislikeable do |options|
+    post 'dislike', action: :create, controller: options[:controller], on: :member
+    delete 'dislike', action: :destroy, controller: options[:controller], on: :member
+  end
+
   scope "(:locale)", locale: /#{ I18n.locales.join("|")}/ do
     devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 
@@ -18,10 +28,8 @@ Rails.application.routes.draw do
       end
 
       resources :posts, concerns: :paginatable do
-        post 'like', to: 'posts_likes#create', on: :member
-        delete 'like', to: 'posts_likes#destroy', on: :member
-        post 'dislike', to: 'posts_dislikes#create', on: :member
-        delete 'dislike', to: 'posts_dislikes#destroy', on: :member
+        concerns :likeable, controller: 'posts_likes'
+        concerns :dislikeable, controller: 'posts_dislikes'
 
         resources :replies, only: [:create, :update, :destroy], concerns: :paginatable
       end
