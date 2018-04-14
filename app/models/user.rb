bc0a23ec1b6e0abe55217ca9aca368762d54cf2a
agustin <ortu.agustin@gmail.com>
2018-04-14 15:20:10 -0300

    removed comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :subscriptions, dependent: :destroy, foreign_key: 'subscriber_id'
   belongs_to :role, optional: true
-  # has_many :permissions, through: :role
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, confirmation: true
