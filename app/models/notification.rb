0e24a27e7125fc014a853ed99fce6dce148b973e
agustin <ortu.agustin@gmail.com>
2018-04-24 16:23:19 -0300

    notifications may be read

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,11 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :receiver, class_name: "User"
   belongs_to :notifiable, polymorphic: true
+
+  scope :read, ->{ where.not read_at: nil }
+  scope :unread, ->{ where read_at: nil }
+
+  def read?
+    read_at.present?
+  end
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -50,4 +50,24 @@ class NotificationTest < ActiveSupport::TestCase
 
     assert receiver.notifications.empty?, 'receiver notifications should still be empty'
   end
+
+  test "notifications may be marked as read" do
+    receiver = @user
+    @post.add_subscription_for receiver
+
+    @post.replies << Reply.new(user: User.last, body: 'test')
+    @post.save
+
+    assert_equal 1, receiver.notifications.size, 'receiver should have 1 notification'
+    assert_equal 1, receiver.unread_notifications.size, 'receiver should have 1 unread notification'
+    assert_equal 0, receiver.read_notifications.size, 'receiver should have 0 read notifications'
+    refute receiver.notifications.first.read?, 'notification should not be marked as read'
+
+    receiver.read_notifications!
+
+    assert_equal 1, receiver.notifications.size, 'receiver should have 1 notification'
+    assert_equal 0, receiver.unread_notifications.size, 'receiver should have 0 unread notifications'
+    assert_equal 1, receiver.read_notifications.size, 'receiver should have 1 read notification'
+    assert receiver.notifications.first.read?, 'notification should be marked as read'
+  end
 end
